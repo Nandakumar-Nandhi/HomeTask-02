8fd504b22826807ae9b75cf3f3baa24850549e4b
Nandakumar-Nandhi <124292594+Nandakumar-Nandhi@users.noreply.github.com>
2023-02-15 10:57:03 +0530

    Commiting code changes to Questions4

--- Daily Tasks- Day 2/Question4.js
+++ Daily Tasks- Day 2/Question4.js
@@ -10,13 +10,39 @@ var x = make(5)(10)(15);
 Assert(x(add) == 30);
 Assert(x(mul) == 750);
 */
-function add(x, y) { return x + y; }
-function mul(x, y) { return x * y; }
-function make(/*???*/) { /* ??? */ }
 
-var s = make(1)(2)(3)(4)(5);
-Assert(s(add) == 15);
-Assert(s(mul) == 120);
-var x = make(5)(10)(15);
-Assert(x(add) == 30);
-Assert(x(mul) == 750);
+var array=[];
+function make(number)
+{
+    if(number===add){
+      return add();
+    }
+    else if(number===mul){
+      return mul();
+    }
+    else if(number){
+      array.push(number);
+    }
+    return function(current){
+       if(current){
+          return make(current);
+       }
+   }
+} 
+function add(){
+  var result=0;
+  for(let i=0;i<array.length;i++){
+    result+=array[i];
+  }
+  return result;
+} 
+function mul(){
+  var result=1;
+  for(let i=0;i<array.length;i++){
+    result*=array[i];
+  }
+  return result;
+} 
+var s=make(1)(2)(3)(4);
+console.log(s(add));
+console.log(s(mul));
